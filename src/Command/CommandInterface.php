1cfc3061dec35718f186a86d4a968fc7dce9010c
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-06-04 13:10:31 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Command/CommandInterface.php
+++ src/Command/CommandInterface.php
@@ -3,8 +3,19 @@ namespace Knight23\Core\Command;
 
 interface CommandInterface
 {
+    /**
+     * @return string
+     */
     public function getHelp();
+
+    /**
+     * @return string
+     */
     public function getShort();
+
+    /**
+     * @return string
+     */
     public function getName();
     public function run(array $options, array $arguments);
 }
